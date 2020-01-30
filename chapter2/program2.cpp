2dca1528d728a2b4ffb8f9cc6b1dfab507039397
vamarajuk <54964344+vamarajuk@users.noreply.github.com>
2020-01-30 00:28:53 -0500

    Create program2.cpp

---
+++ chapter2/program2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	int x;
+	x = 1023;
+
+	cout << "This program prints the value of x: ";
+	cout << x << endl;
+
+	system("pause");
+	return 0;
+}
