f7a293b320cb8907d28f39c1e95c9d20bc80a1c3
vamarajuk <54964344+vamarajuk@users.noreply.github.com>
2020-01-30 00:29:18 -0500

    Create program3.cpp

---
+++ chapter2/program3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	int gallons, liters;
+	
+
+	cout << "enter number of gallons: " << endl;
+	cin >> gallons; 
+
+	liters = gallons * 4;
+
+	cout << "liters: " << liters << endl;
+
+
+	system("pause");
+	return 0;
+}
