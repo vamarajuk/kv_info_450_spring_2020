b141bd63a1b0a0e01d945dbf1e3a34b63378e653
vamarajuk <54964344+vamarajuk@users.noreply.github.com>
2020-03-04 16:49:45 -0500

    Update program1.cpp

--- week7/program1.cpp
+++ week7/program1.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 using namespace std;
 string reverse(string *s1);
 
 int main()
 {
-	string s1;
+	string s1, output;
 	cout << "enter string to reverse." << endl;
 	getline(cin, s1);
-	reverse(&s1);
-	cout << s1 << endl;
+	output = reverse(&s1);
+	cout << output << endl;
 
 
 	system("pause");
@@ -19,19 +20,32 @@ int main()
 
 string reverse(string *s1)
 {
-	string s2;
-	//int L1 = s1->length;
-	/*for (unsigned i = 0; i < s1->length(); i++)
+	//string s2;
+	char temp1[1024];
+	char temp2[1024];
+	strncpy_s(temp1, s1->c_str(), s1->length());
+	int x = 0;
+	for (int i = strlen(temp1); i>-1; i-- )
 	{
-		s2 = s2 + s1->at(s1->length() - i);
-		cout << i << endl;//error checking
-	}*/
+		//strcat(temp2, temp1[i]);
 
-	for (int i = 0; i < s1->length(); ++i)
+		temp2[x] = temp1[i];
+		cout << temp2[x] << endl;
+		x++;
+		cout << "i:" << i << " x:" << x << endl;
+		
+		cout << temp2 << endl;
+	}
+	
+	cout << temp2 << endl;
+	string s2(temp2);
+	cout << s2 << endl;
+
+	/*for (unsigned i = 0; i < s1->length(); ++i)
 	{
 		s2.insert(0, s1[i]);
-		cout << i << endl;//error checking
-	}
+		cout << i << endl;
+	}*/
 
 	return s2;
 }
