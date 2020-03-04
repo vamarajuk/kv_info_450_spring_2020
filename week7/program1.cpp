b7d0e40fb1d57ef4daa26dc3e021b3cdccdc46ca
vamarajuk <54964344+vamarajuk@users.noreply.github.com>
2020-03-04 18:45:09 -0500

    Update program1.cpp

--- week7/program1.cpp
+++ week7/program1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <cstring>
 
 using namespace std;
 string reverse(string *s1);
@@ -8,11 +8,20 @@ string reverse(string *s1);
 int main()
 {
 	string s1, output;
+	int stop = 0;
+	do
+	{
 	cout << "enter string to reverse." << endl;
 	getline(cin, s1);
+	if(s1=="")
+	{
+	    stop = 1;
+	    break;
+	}
 	output = reverse(&s1);
 	cout << output << endl;
-
+	}while(stop = 0);
+	
 
 	system("pause");
 	return 0;
@@ -20,26 +29,27 @@ int main()
 
 string reverse(string *s1)
 {
-	//string s2;
+	
 	char temp1[1024];
 	char temp2[1024];
-	strncpy_s(temp1, s1->c_str(), s1->length());
+	strncpy(temp1, s1->c_str(), s1->length());
 	int x = 0;
-	for (int i = strlen(temp1); i>-1; i-- )
+	for (int i = (strlen(temp1)-1); i>-1; i-- )
 	{
-		//strcat(temp2, temp1[i]);
+	
 
 		temp2[x] = temp1[i];
-		cout << temp2[x] << endl;
-		x++;
-		cout << "i:" << i << " x:" << x << endl;
 		
-		cout << temp2 << endl;
+		x++;
+		//cout << "i:" << i << " x:" << x << endl;
+		//error checking
+		
 	}
 	
-	cout << temp2 << endl;
+	temp2[strlen(temp2)] = '\0';
+	
 	string s2(temp2);
-	cout << s2 << endl;
+	
 
 	/*for (unsigned i = 0; i < s1->length(); ++i)
 	{
